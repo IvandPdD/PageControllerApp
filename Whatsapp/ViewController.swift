5d8dd49c003f41a7c13a09515e5e8e9e31b9f165
iCreSTZ <forzicrestz@gmail.com>
2021-02-11 21:12:19 +0100

    audio recorder

--- Whatsapp/ViewController.swift
+++ Whatsapp/ViewController.swift
@@ -7,14 +7,83 @@
 //
 
 import UIKit
+import SwiftUI
+import AVKit
+
 
 class ViewController: UIViewController {
+    
+    var longgesture : UILongPressGestureRecognizer?
+    var url: URL?
+    var grabador: AVAudioRecorder?
+    var reproductor: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        micButton.setImage(UIImage(named: "Mic Off"), for: .normal)
+        
+        micButton.addTarget(self, action: #selector(ViewController.touchDownEvent), for: .touchDown)
+        micButton.addTarget(self, action: #selector(ViewController.touchUpEvent), for: [.touchUpInside, .touchUpOutside])
+
+        let fichero = "grabacion.wav"
+
+        let directorio = FileManager.default.urls(for:
+        .documentDirectory, in: .userDomainMask)
+        url = directorio.first?.appendingPathComponent(fichero)
+        
+        let sesion = AVAudioSession.sharedInstance()
+        try! sesion.setCategory(AVAudioSession.Category.playAndRecord)
+        
+    }
+
+    @IBOutlet weak var micButton: UIButton!
+    @IBOutlet weak var textField: UITextView!
+    
+    @IBAction func resetButton(_ sender: Any) {
+    }
+    
+    @objc func touchDownEvent(_ sender: AnyObject) {
+        micButton.setImage(UIImage(named: "Mic"), for: .normal)
+        
+        //TODO start recording
+
+        grabador = try! AVAudioRecorder(url: url!, settings: [:])
+        grabador!.prepareToRecord()
+        grabador!.record()
+        
+    }
+
+    @objc func touchUpEvent(_ sender: AnyObject) {
+        micButton.setImage(UIImage(named: "Mic Off"), for: .normal)
+        
+        //TODO stop recording
+        grabador!.stop()
+        let audioPath = (NSSearchPathForDirectoriesInDomains(.documentDirectory
+        , .userDomainMask, true)[0] as
+        NSString).appendingPathComponent("grabacion.wav")
+        textField.text = audioPath
+        
+        var url = URL(string: audioPath)
+        do{
+            reproductor = try AVAudioPlayer(contentsOf: url!)
+
+            reproductor!.prepareToPlay() // Precarga de canción
+            reproductor!.play()
+        }catch{}
+        
+        
     }
 
 
+
+    
 }
 
+
+struct ViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
